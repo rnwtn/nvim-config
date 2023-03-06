7833c4eb2d9f1a7f245da64091de8e728aa151df
krankykong <77465934+krankykong@users.noreply.github.com>
2023-03-05 21:58:24 -0600

    Remove comments

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -33,7 +33,7 @@ return require('packer').startup(function(use)
   use 'navarasu/onedark.nvim'
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 
-  -- LSP support
+  -- LSP setup
   use {
     'neovim/nvim-lspconfig',
     requires = {
@@ -46,9 +46,9 @@ return require('packer').startup(function(use)
     }
   }
 
-  -- Autocompletion and Snippets
+  -- Autocompletion and snippets
   use {
-    'hrsh7th/nvim-cmp', -- Required
+    'hrsh7th/nvim-cmp',
     requires = {
       { 'hrsh7th/cmp-nvim-lsp' }, -- Completions from Neovim's built-in LSP client
       { 'hrsh7th/cmp-buffer' }, -- Completions from text in buffer
@@ -81,7 +81,7 @@ return require('packer').startup(function(use)
     branch = 'v2.x',
     requires = {
       'nvim-lua/plenary.nvim',
-      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'nvim-tree/nvim-web-devicons',
       'MunifTanjim/nui.nvim',
     }
   }
