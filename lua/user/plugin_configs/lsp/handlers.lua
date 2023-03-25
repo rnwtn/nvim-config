5799d45e40321fc00c9724c120379c749e782ef5
krankykong <77465934+krankykong@users.noreply.github.com>
2023-03-24 20:58:58 -0500

    Disable formatting for lua_ls

--- lua/user/plugin_configs/lsp/handlers.lua
+++ lua/user/plugin_configs/lsp/handlers.lua
@@ -108,6 +108,9 @@ M.on_attach = function(client, bufnr)
   if client.name == "tsserver" then
     client.server_capabilities.document_formatting = false
   end
+  if client.name == "lua_ls" then
+    client.server_capabilities.document_formatting = false
+  end
   lsp_keymaps(client, bufnr)
   lsp_highlight_document(client)
 end
