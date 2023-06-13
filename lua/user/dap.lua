419778e73c33af262d605e530d2fe39748f9cc12
krankykong <77465934+krankykong@users.noreply.github.com>
2023-06-12 23:51:25 -0500

    Configure DAP

--- lua/user/dap.lua
+++ lua/user/dap.lua
@@ -1,6 +1,13 @@
 local M = {
   "mfussenegger/nvim-dap",
   event = "VeryLazy",
+  dependencies = {
+    "williamboman/mason.nvim", -- Easily install debug adapters
+    "rcarriga/nvim-dap-ui", -- A more user-friendly UI for nvim-dap
+    "theHamsta/nvim-dap-virtual-text", -- Debug information displayed alongside code
+    "mxsdev/nvim-dap-vscode-js", -- Easily setup DAP for javascript/typescript debugging
+    "David-Kunz/jester", -- Run and debug individual jest tests (Javascript/Typescript)
+  },
 }
 
 function M.config()
@@ -23,34 +30,115 @@ function M.config()
     dapui.close()
   end
 
-  dap.adapters.codelldb = {
+  -- Setup adapters
+  dap.adapters.codelldb = function(cb, config)
+    if config.preLaunchTask then
+      vim.notify "Performing pre-launch tasks"
+      config.preLaunchTask()
+    end
+    local adapter = {
+      type = "server",
+      port = "${port}",
+      executable = {
+        command = "codelldb",
+        args = { "--port", "${port}" },
+      },
+    }
+    cb(adapter)
+  end
+  dap.adapters.coreclr = function(cb, config)
+    if config.preLaunchTask then
+      vim.notify "Performing pre-launch tasks"
+      config.preLaunchTask()
+    end
+    local adapter = {
+      type = "executable",
+      command = "netcoredbg",
+      args = { "--interpreter=vscode" },
+    }
+    cb(adapter)
+  end
+  dap.adapters.godot = {
     type = "server",
-    port = "${port}",
-    executable = {
-      -- provide the absolute path for `codelldb` command if not using the one installed using `mason.nvim`
-      command = "codelldb",
-      args = { "--port", "${port}" },
-      -- On windows you may have to uncomment this:
-      -- detached = false,
-    },
+    host = "127.0.0.1",
+    port = 6006,
   }
-  dap.configurations.c = {
+
+  require("dap-vscode-js").setup {
+    debugger_path = vim.fn.stdpath "data" .. "/mason/packages/js-debug-adapter",
+    debugger_cmd = { "js-debug-adapter" },
+    adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" },
+  }
+
+  -- Setup default launch configurations
+  local codelldb_config = {
     {
       name = "Launch file",
       type = "codelldb",
       request = "launch",
       program = function()
-        local path
-        vim.ui.input({ prompt = "Path to executable: ", default = vim.loop.cwd() .. "/build/" }, function(input)
-          path = input
-        end)
-        vim.cmd [[redraw]]
-        return path
+        return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
       end,
       cwd = "${workspaceFolder}",
       stopOnEntry = false,
     },
   }
+  local node_config = {
+    {
+      type = "pwa-node",
+      request = "launch",
+      name = "Launch",
+      cwd = "${workspaceFolder}",
+      program = "${workspaceFolder}/dist/index.js",
+      detached = false,
+      skipFiles = { "<node_internals>/**" },
+      resolveSourceMapLocations = {
+        "${workspaceFolder}/dist/**/*.js",
+        "${workspaceFolder}/dist/*.js",
+      },
+    },
+    {
+      type = "pwa-node",
+      request = "attach",
+      name = "Attach",
+      cwd = "${workspaceFolder}",
+      processId = require("dap.utils").pick_process,
+      skipFiles = { "<node_internals>/**" },
+      resolveSourceMapLocations = {
+        "${workspaceFolder}/dist/**/*.js",
+        "${workspaceFolder}/dist/*.js",
+      },
+    },
+  }
+
+  dap.configurations.cs = {
+    {
+      type = "coreclr",
+      name = "launch - netcoredbg",
+      request = "launch",
+      program = function()
+        return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file")
+      end,
+    },
+  }
+  dap.configurations.gdscript = {
+    {
+      type = "godot",
+      request = "launch",
+      name = "Launch scene",
+      project = "${workspaceFolder}",
+      launch_scene = true,
+    },
+  }
+
+  dap.configurations.cpp = codelldb_config
+  dap.configurations.c = codelldb_config
+  dap.configurations.rust = codelldb_config
+  dap.configurations.typescript = node_config
+  dap.configurations.javascript = node_config
+
+  -- Set virtual text when debugging
+  require("nvim-dap-virtual-text").setup {}
 end
 
 return M
