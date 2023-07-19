48121b98919f5dc1e71c27a94037c625e9740655
Rhett Newton <77465934+rnwtn@users.noreply.github.com>
2023-07-19 05:21:39 -0500

    Add everforest colorscheme

--- lua/user/plugins/colorscheme.lua
+++ lua/user/plugins/colorscheme.lua
@@ -1,66 +1,66 @@
 return {
-  "ellisonleao/gruvbox.nvim",
-  name = "gruvbox",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    local dimmed_bg = "#22201f"
-    local telescope_bg = "#32302f"
-    local telescope_bg2 = "#3c3836"
-    local telescope_fg = "#f2e5bc"
-    local telescope_green = "#b8bb26"
-    local telescope_red = "#fb4934"
+  {
+    "ellisonleao/gruvbox.nvim",
+    name = "gruvbox",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      local dimmed_bg = "#22201f"
+      local telescope_bg = "#32302f"
+      local telescope_bg2 = "#3c3836"
+      local telescope_fg = "#f2e5bc"
+      local telescope_green = "#b8bb26"
+      local telescope_red = "#fb4934"
 
-    require("gruvbox").setup {
-      undercurl = true,
-      underline = true,
-      bold = true,
-      italic = {
-        string = true,
-        comments = true,
-        operators = false,
-        folds = true,
-      },
-      strikethrough = true,
-      invert_selection = false,
-      invert_signs = false,
-      invert_tabline = false,
-      invert_intend_guides = false,
-      inverse = true, -- invert background for search, diffs, statuslines and errors
-      contrast = "", -- can be "hard", "soft" or empty string
-      palette_overrides = {},
-      overrides = {
-        -- oil.nvim
-        NormalFloat = { bg = telescope_bg },
-        FloatBorder = { bg = telescope_bg, fg = telescope_fg },
-
-        -- Telescope
-        TelescopeBorder = { fg = telescope_bg, bg = telescope_bg },
-        TelescopeNormal = { bg = telescope_bg2 },
-        TelescopePreviewBorder = { fg = telescope_bg, bg = telescope_bg },
-        TelescopePreviewNormal = { bg = telescope_bg },
-        TelescopePreviewTitle = { fg = telescope_bg, bg = telescope_green },
-        TelescopePromptBorder = { fg = telescope_bg2, bg = telescope_bg2 },
-        TelescopePromptNormal = { fg = telescope_fg, bg = telescope_bg2 },
-        TelescopePromptPrefix = { fg = telescope_red, bg = telescope_bg2 },
-        TelescopePromptTitle = { fg = telescope_bg, bg = telescope_red },
-        TelescopeResultsBorder = { fg = telescope_bg, bg = telescope_bg },
-        TelescopeResultsNormal = { bg = telescope_bg },
-        TelescopeResultsTitle = { fg = telescope_bg, bg = telescope_bg },
-
-        -- NeoTree
-        NeoTreeNormal = { bg = dimmed_bg },
-        NeoTreeNormalNC = { bg = dimmed_bg },
-
-        -- Haropoon Tabline
-        TabLineFill = { bg = dimmed_bg },
-        HarpoonInactive = { bg = dimmed_bg },
-        HarpoonNumberInactive = { bg = dimmed_bg },
-        HarpoonActive = { bg = "NONE" },
-        HarpoonNumberActive = { bg = "NONE" },
-      },
-      dim_inactive = false,
-      transparent_mode = false,
-    }
-  end,
+      require("gruvbox").setup {
+        invert_selection = true,
+        overrides = {
+          NormalFloat = { bg = telescope_bg },
+          FloatBorder = { bg = telescope_bg, fg = telescope_fg },
+          TabLineFill = { bg = dimmed_bg },
+          TelescopeBorder = { fg = telescope_bg, bg = telescope_bg },
+          TelescopeNormal = { bg = telescope_bg2 },
+          TelescopePreviewBorder = { fg = telescope_bg, bg = telescope_bg },
+          TelescopePreviewNormal = { bg = telescope_bg },
+          TelescopePreviewTitle = { fg = telescope_bg, bg = telescope_green },
+          TelescopePromptBorder = { fg = telescope_bg2, bg = telescope_bg2 },
+          TelescopePromptNormal = { fg = telescope_fg, bg = telescope_bg2 },
+          TelescopePromptPrefix = { fg = telescope_red, bg = telescope_bg2 },
+          TelescopePromptTitle = { fg = telescope_bg, bg = telescope_red },
+          TelescopeResultsBorder = { fg = telescope_bg, bg = telescope_bg },
+          TelescopeResultsNormal = { bg = telescope_bg },
+          TelescopeResultsTitle = { fg = telescope_bg, bg = telescope_bg },
+          HarpoonInactive = { bg = dimmed_bg },
+          HarpoonNumberInactive = { bg = dimmed_bg },
+          HarpoonActive = { bg = "NONE" },
+          HarpoonNumberActive = { bg = "NONE" },
+        },
+      }
+    end,
+  },
+  {
+    "neanias/everforest-nvim",
+    version = false,
+    lazy = false,
+    priority = 1000,
+    opts = {
+      ui_contrast = "high",
+      on_highlights = function(hl, palette)
+        hl.NormalFloat = { bg = palette.none }
+        hl.FloatBorder = { bg = palette.none, fg = palette.fg }
+        hl.TelescopeBorder = { fg = palette.none, bg = palette.none }
+        hl.TelescopeNormal = { bg = palette.grey1 }
+        hl.TelescopePreviewBorder = { fg = palette.bg1, bg = palette.bg1 }
+        hl.TelescopePreviewNormal = { bg = palette.bg1 }
+        hl.TelescopePreviewTitle = { fg = palette.none, bg = palette.green }
+        hl.TelescopePromptBorder = { fg = palette.bg2, bg = palette.bg2 }
+        hl.TelescopePromptNormal = { fg = palette.fg, bg = palette.bg2 }
+        hl.TelescopePromptPrefix = { fg = palette.red, bg = palette.bg2 }
+        hl.TelescopePromptTitle = { fg = palette.none, bg = palette.red }
+        hl.TelescopeResultsBorder = { fg = palette.bg1, bg = palette.bg1 }
+        hl.TelescopeResultsNormal = { bg = palette.bg1 }
+        hl.TelescopeResultsTitle = { fg = palette.bg1, bg = palette.bg1 }
+      end,
+    },
+  },
 }
