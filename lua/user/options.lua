7995c03ee7d8284acf98c9d24b11e3e8a7f4b7a4
Rhett Newton <77465934+rnwtn@users.noreply.github.com>
2023-07-19 23:36:08 -0500

    Remove commented code

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -9,7 +9,6 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.pumheight = 15
 vim.opt.showmode = false
--- vim.opt.showtabline = 0
 vim.opt.smartindent = true
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
@@ -27,8 +26,3 @@ vim.opt.scrolloff = 8
 vim.opt.sidescrolloff = 8
 vim.opt.iskeyword:append "-"
 vim.opt.whichwrap:append "<,>,[,],h,l"
--- vim.opt.completeopt = { "menuone", "noselect" }
--- vim.opt.formatoptions:remove { "c", "r", "o" } -- This is a sequence of letters which describes how automatic formatting is to be done
--- vim.opt.shortmess:append "c" -- hide all the completion messages, e.g. "-- XXX completion (YYY)", "match 1 of 2", "The only match", "Pattern not found"
--- vim.opt.fillchars.eob = " " -- show empty lines at the end of a buffer as ` ` {default `~`}
--- vim.opt.showcmd = false -- hide (partial) command in the last line of the screen (for performance)
