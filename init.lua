e733da3f103c5abbf68d9ae6b7778ae3fe85a8a6
Rhett Newton <77465934+rnwtn@users.noreply.github.com>
2023-07-19 23:44:48 -0500

    Add local leader declaration

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@
 -- vi vi vi - the editor of the beast --
 ----------------------------------------
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 require "user.plugin_manager"
 require "user.options"
