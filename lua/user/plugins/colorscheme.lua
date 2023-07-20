04b09b694014812751e2f72488b09e6b3f2cf936
Rhett Newton <77465934+rnwtn@users.noreply.github.com>
2023-07-19 22:14:59 -0500

    Add onedark

--- lua/user/plugins/colorscheme.lua
+++ lua/user/plugins/colorscheme.lua
@@ -65,4 +65,64 @@ return {
       }
     end,
   },
+  {
+    "navarasu/onedark.nvim",
+    name = "onedark",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("onedark").setup {
+        -- Main options --
+        style = "darker", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+        transparent = false, -- Show/hide background
+        term_colors = true, -- Change terminal color as per the selected theme style
+        ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
+        cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
+
+        -- toggle theme style ---
+        toggle_style_key = "<leader>ts", -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+        toggle_style_list = { "dark", "darker", "cool", "deep", "warm", "warmer", "light" }, -- List of styles to toggle between
+
+        -- Change code style ---
+        -- Options are italic, bold, underline, none
+        -- You can configure multiple style with comma seperated, For e.g., keywords = 'italic,bold'
+        code_style = {
+          comments = "italic",
+          keywords = "none",
+          functions = "none",
+          strings = "none",
+          variables = "none",
+        },
+
+        -- Lualine options --
+        lualine = {
+          transparent = false, -- lualine center bar transparency
+        },
+
+        -- Custom Highlights --
+        colors = {}, -- Override default colors
+        highlights = {
+          TelescopeBorder = { fg = "$bg1", bg = "$bg1" },
+          TelescopeNormal = { bg = "bg2" },
+          TelescopePreviewBorder = { fg = "$bg1", bg = "$bg1" },
+          TelescopePreviewNormal = { bg = "$bg1" },
+          TelescopePreviewTitle = { fg = "$bg1", bg = "$green" },
+          TelescopePromptBorder = { fg = "$bg2", bg = "$bg2" },
+          TelescopePromptNormal = { fg = "$fg", bg = "$bg2" },
+          TelescopePromptPrefix = { fg = "$red", bg = "$bg2" },
+          TelescopePromptTitle = { fg = "$bg1", bg = "$red" },
+          TelescopeResultsBorder = { fg = "$bg1", bg = "$bg1" },
+          TelescopeResultsNormal = { bg = "$bg1" },
+          TelescopeResultsTitle = { fg = "$bg1", bg = "$bg1" },
+        },
+
+        -- Plugins Config --
+        diagnostics = {
+          darker = true, -- darker colors for diagnostic
+          undercurl = true, -- use undercurl instead of underline for diagnostics
+          background = true, -- use background color for virtual text
+        },
+      }
+    end,
+  },
 }
