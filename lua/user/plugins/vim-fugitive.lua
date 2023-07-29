f0efb458f4ea29d4b2b4cdf8a10a1fd133bc6f95
Rhett Newton <77465934+rnwtn@users.noreply.github.com>
2023-07-28 23:05:28 -0500

    Add keymaps

--- lua/user/plugins/vim-fugitive.lua
+++ lua/user/plugins/vim-fugitive.lua
@@ -2,5 +2,10 @@ return {
   "tpope/vim-fugitive",
   event = "VimEnter",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  -- opts = {},
+  keys = {
+    { "<leader>gs", ":Git<cr>", desc = "Git open Vim-Fugitive" },
+    { "<leader>gp", ":Git pull<cr>", desc = "Git pull" },
+    { "<leader>gP", ":Git push<cr>", desc = "Git push" },
+    { "<leader>gc", ":Git commit<cr>", desc = "Git push" },
+  },
 }
