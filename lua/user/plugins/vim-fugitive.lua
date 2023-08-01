4bedde2e3873bfb0cdd958a9e8df065dc7415cab
Rhett Newton <77465934+rnwtn@users.noreply.github.com>
2023-07-31 23:02:55 -0500

    Update to open fugitive in split window

--- lua/user/plugins/vim-fugitive.lua
+++ lua/user/plugins/vim-fugitive.lua
@@ -3,15 +3,16 @@ return {
   event = "VimEnter",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   keys = {
-    {
-      "<leader>gs",
-      function()
-        vim.cmd "tabnew"
-        vim.cmd "Git"
-        vim.cmd ":only"
-      end,
-      desc = "Git open Vim-Fugitive",
-    },
+    -- {
+    --   "<leader>gs",
+    --   function()
+    --     vim.cmd "tabnew"
+    --     vim.cmd "Git"
+    --     vim.cmd ":only"
+    --   end,
+    --   desc = "Git open Vim-Fugitive",
+    -- },
+    { "<leader>gs", ":Git<cr>",        desc = "Git open Vim-Fugitive" },
     { "<leader>gp", ":Git pull<cr>",   desc = "Git pull" },
     { "<leader>gP", ":Git push<cr>",   desc = "Git push" },
     { "<leader>gc", ":Git commit<cr>", desc = "Git push" },
