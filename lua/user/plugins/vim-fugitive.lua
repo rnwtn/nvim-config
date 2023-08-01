0512d9f646bd20865f80feec1a13087b73e872b7
Rhett Newton <77465934+rnwtn@users.noreply.github.com>
2023-07-31 22:56:52 -0500

    Make fugitive open in new tab

--- lua/user/plugins/vim-fugitive.lua
+++ lua/user/plugins/vim-fugitive.lua
@@ -3,9 +3,17 @@ return {
   event = "VimEnter",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   keys = {
-    { "<leader>gs", ":Git<cr>", desc = "Git open Vim-Fugitive" },
-    { "<leader>gp", ":Git pull<cr>", desc = "Git pull" },
-    { "<leader>gP", ":Git push<cr>", desc = "Git push" },
+    {
+      "<leader>gs",
+      function()
+        vim.cmd "tabnew"
+        vim.cmd "Git"
+        vim.cmd ":only"
+      end,
+      desc = "Git open Vim-Fugitive",
+    },
+    { "<leader>gp", ":Git pull<cr>",   desc = "Git pull" },
+    { "<leader>gP", ":Git push<cr>",   desc = "Git push" },
     { "<leader>gc", ":Git commit<cr>", desc = "Git push" },
   },
 }
