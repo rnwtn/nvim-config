d54ca894e445927b37980fec08a65fcc626d7ed9
Rhett Newton <77465934+rnwtn@users.noreply.github.com>
2023-08-03 14:09:29 -0500

    Simplify custom highlights

--- lua/user/plugins/colorscheme.lua
+++ lua/user/plugins/colorscheme.lua
@@ -5,12 +5,7 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      local dimmed_bg = "#22201f"
-      local telescope_bg = "#32302f"
-      local telescope_bg2 = "#3c3836"
-      local telescope_fg = "#f2e5bc"
-      local telescope_green = "#b8bb26"
-      local telescope_red = "#fb4934"
+      local fg = "#f2e5bc"
 
       require("gruvbox").setup {
         invert_selection = true,
@@ -18,25 +13,7 @@ return {
         overrides = {
           Normal = { bg = "NONE" },
           NormalFloat = { bg = "NONE" },
-          -- NormalFloat = { bg = telescope_bg },
-          FloatBorder = { bg = "NONE", fg = telescope_fg },
-          -- TabLineFill = { bg = dimmed_bg },
-          -- TelescopeBorder = { fg = telescope_bg, bg = telescope_bg },
-          -- TelescopeNormal = { bg = telescope_bg2 },
-          -- TelescopePreviewBorder = { fg = telescope_bg, bg = telescope_bg },
-          -- TelescopePreviewNormal = { bg = telescope_bg },
-          -- TelescopePreviewTitle = { fg = telescope_bg, bg = telescope_green },
-          -- TelescopePromptBorder = { fg = telescope_bg2, bg = telescope_bg2 },
-          -- TelescopePromptNormal = { fg = telescope_fg, bg = telescope_bg2 },
-          -- TelescopePromptPrefix = { fg = telescope_red, bg = telescope_bg2 },
-          -- TelescopePromptTitle = { fg = telescope_bg, bg = telescope_red },
-          -- TelescopeResultsBorder = { fg = telescope_bg, bg = telescope_bg },
-          -- TelescopeResultsNormal = { bg = telescope_bg },
-          -- TelescopeResultsTitle = { fg = telescope_bg, bg = telescope_bg },
-          -- HarpoonInactive = { bg = dimmed_bg },
-          -- HarpoonNumberInactive = { bg = dimmed_bg },
-          -- HarpoonActive = { bg = "NONE" },
-          -- HarpoonNumberActive = { bg = "NONE" },
+          FloatBorder = { bg = "NONE", fg = fg },
         },
       }
     end,
@@ -49,21 +26,10 @@ return {
     config = function()
       require("everforest").setup {
         ui_contrast = "high",
+        transparent_background_level = 2,
         on_highlights = function(hl, palette)
           hl.NormalFloat = { bg = palette.none }
           hl.FloatBorder = { bg = palette.none, fg = palette.fg }
-          hl.TelescopeBorder = { fg = palette.none, bg = palette.none }
-          hl.TelescopeNormal = { bg = palette.grey1 }
-          hl.TelescopePreviewBorder = { fg = palette.bg1, bg = palette.bg1 }
-          hl.TelescopePreviewNormal = { bg = palette.bg1 }
-          hl.TelescopePreviewTitle = { fg = palette.none, bg = palette.green }
-          hl.TelescopePromptBorder = { fg = palette.bg2, bg = palette.bg2 }
-          hl.TelescopePromptNormal = { fg = palette.fg, bg = palette.bg2 }
-          hl.TelescopePromptPrefix = { fg = palette.red, bg = palette.bg2 }
-          hl.TelescopePromptTitle = { fg = palette.none, bg = palette.red }
-          hl.TelescopeResultsBorder = { fg = palette.bg1, bg = palette.bg1 }
-          hl.TelescopeResultsNormal = { bg = palette.bg1 }
-          hl.TelescopeResultsTitle = { fg = palette.bg1, bg = palette.bg1 }
         end,
       }
     end,
@@ -105,18 +71,8 @@ return {
         -- Custom Highlights --
         colors = {}, -- Override default colors
         highlights = {
-          TelescopeBorder = { fg = "$bg1", bg = "$bg1" },
-          TelescopeNormal = { bg = "bg2" },
-          TelescopePreviewBorder = { fg = "$bg1", bg = "$bg1" },
-          TelescopePreviewNormal = { bg = "$bg1" },
-          TelescopePreviewTitle = { fg = "$bg1", bg = "$green" },
-          TelescopePromptBorder = { fg = "$bg2", bg = "$bg2" },
-          TelescopePromptNormal = { fg = "$fg", bg = "$bg2" },
-          TelescopePromptPrefix = { fg = "$red", bg = "$bg2" },
-          TelescopePromptTitle = { fg = "$bg1", bg = "$red" },
-          TelescopeResultsBorder = { fg = "$bg1", bg = "$bg1" },
-          TelescopeResultsNormal = { bg = "$bg1" },
-          TelescopeResultsTitle = { fg = "$bg1", bg = "$bg1" },
+          NormalFloat = { bg = "none" },
+          FloatBorder = { bg = "none", fg = "$fg" },
         },
 
         -- Plugins Config --
@@ -129,3 +85,28 @@ return {
     end,
   },
 }
+
+-- For NvChad like telescope theme, do something like the following
+--local dimmed_bg = "#22201f"
+--local telescope_bg = "#32302f"
+--local telescope_bg2 = "#3c3836"
+--local telescope_fg = "#f2e5bc"
+--local telescope_green = "#b8bb26"
+--local telescope_red = "#fb4934"
+--TabLineFill = { bg = dimmed_bg },
+--TelescopeBorder = { fg = telescope_bg, bg = telescope_bg },
+--TelescopeNormal = { bg = telescope_bg2 },
+--TelescopePreviewBorder = { fg = telescope_bg, bg = telescope_bg },
+--TelescopePreviewNormal = { bg = telescope_bg },
+--TelescopePreviewTitle = { fg = telescope_bg, bg = telescope_green },
+--TelescopePromptBorder = { fg = telescope_bg2, bg = telescope_bg2 },
+--TelescopePromptNormal = { fg = telescope_fg, bg = telescope_bg2 },
+--TelescopePromptPrefix = { fg = telescope_red, bg = telescope_bg2 },
+--TelescopePromptTitle = { fg = telescope_bg, bg = telescope_red },
+--TelescopeResultsBorder = { fg = telescope_bg, bg = telescope_bg },
+--TelescopeResultsNormal = { bg = telescope_bg },
+--TelescopeResultsTitle = { fg = telescope_bg, bg = telescope_bg },
+--HarpoonInactive = { bg = dimmed_bg },
+--HarpoonNumberInactive = { bg = dimmed_bg },
+--HarpoonActive = { bg = "NONE" },
+--HarpoonNumberActive = { bg = "NONE" },
