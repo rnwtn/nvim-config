fce9f6159aea0c30609173ce2ffcf9bf01b3aa73
Rhett Newton <77465934+rnwtn@users.noreply.github.com>
2023-08-09 23:45:06 -0500

    Add keymap to exist help with q

--- lua/user/autocmds.lua
+++ lua/user/autocmds.lua
@@ -11,3 +11,11 @@ vim.api.nvim_create_autocmd({ "TextYankPost" }, {
     vim.highlight.on_yank { higroup = "Visual", timeout = 200 }
   end,
 })
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "help" },
+  callback = function()
+    local opts = { buffer = true, noremap = true, silent = true }
+    vim.keymap.set("n", "q", ":q<cr>", opts)
+  end,
+})
