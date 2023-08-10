e966679ad8233e273c1db6e0cf0743a137e362a5
Rhett Newton <77465934+rnwtn@users.noreply.github.com>
2023-08-09 23:03:14 -0500

    Add keymap to close trouble

--- lua/user/plugins/trouble.lua
+++ lua/user/plugins/trouble.lua
@@ -2,7 +2,16 @@ return {
   "folke/trouble.nvim",
   event = "LspAttach",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {},
+  config = function()
+    require("trouble").setup({})
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "Trouble",
+      callback = function()
+        local opts = { buffer = true, noremap = true, silent = true }
+        vim.keymap.set("n", "<leader>lD", ":q<cr>", opts)
+      end,
+    })
+  end,
   keys = {
     { "<leader>lD", ":Trouble<cr>", desc = "Trouble toggle"}
   }
