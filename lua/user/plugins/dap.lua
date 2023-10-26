465261215e5213f0785e988211d327d6955e082a
rnwtn <77465934+rnwtn@users.noreply.github.com>
2023-10-26 12:55:40 -0500

    Add nodejs debugging

--- lua/user/plugins/dap.lua
+++ lua/user/plugins/dap.lua
@@ -5,9 +5,15 @@ return {
     "williamboman/mason.nvim",
     "rcarriga/nvim-dap-ui",
     "theHamsta/nvim-dap-virtual-text",
-    "mxsdev/nvim-dap-vscode-js",
-    "David-Kunz/jester",
     "simrat39/rust-tools.nvim",
+    "David-Kunz/jester",
+    "mxsdev/nvim-dap-vscode-js",
+    -- build debugger from source
+    {
+      "microsoft/vscode-js-debug",
+      version = "1.x",
+      build = "npm i && npm run compile vsDebugServerBundle && mv dist out",
+    },
   },
   config = function()
     local dap = require "dap"
@@ -31,52 +37,10 @@ return {
     dap.adapters.coreclr = require "user.dap.adapters.coreclr"
     dap.adapters.godot = require "user.dap.adapters.godot"
 
-    require("dap-vscode-js").setup {
-      debugger_path = vim.fn.stdpath "data" .. "/mason/packages/js-debug-adapter",
-      debugger_cmd = { "js-debug-adapter" },
-      adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" },
-    }
-
-    -- Setup default launch configurations
-    local codelldb_config = {
-      {
-        name = "Launch file",
-        type = "codelldb",
-        request = "launch",
-        program = function()
-          return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-        end,
-        cwd = "${workspaceFolder}",
-        stopOnEntry = false,
-      },
-    }
-    local node_config = {
-      {
-        type = "pwa-node",
-        request = "launch",
-        name = "Launch",
-        cwd = "${workspaceFolder}",
-        program = "${workspaceFolder}/dist/index.js",
-        detached = false,
-        skipFiles = { "<node_internals>/**" },
-        resolveSourceMapLocations = {
-          "${workspaceFolder}/dist/**/*.js",
-          "${workspaceFolder}/dist/*.js",
-        },
-      },
-      {
-        type = "pwa-node",
-        request = "attach",
-        name = "Attach",
-        cwd = "${workspaceFolder}",
-        processId = require("dap.utils").pick_process,
-        skipFiles = { "<node_internals>/**" },
-        resolveSourceMapLocations = {
-          "${workspaceFolder}/dist/**/*.js",
-          "${workspaceFolder}/dist/*.js",
-        },
-      },
-    }
+    require("dap-vscode-js").setup({
+				debugger_path = vim.fn.stdpath("data") .. "/lazy/vscode-js-debug",
+				adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' },
+			})
 
     dap.configurations.cs = {
       {
@@ -97,12 +61,75 @@ return {
         launch_scene = true,
       },
     }
-
-    dap.configurations.cpp = codelldb_config
-    dap.configurations.c = codelldb_config
-    dap.configurations.rust = codelldb_config
-    dap.configurations.typescript = node_config
-    dap.configurations.javascript = node_config
+    for _, language in ipairs { "cpp", "c", "rust" } do
+      require("dap").configurations[language] = {
+        {
+          name = "Launch file",
+          type = "codelldb",
+          request = "launch",
+          program = function()
+            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+          end,
+          cwd = "${workspaceFolder}",
+          stopOnEntry = false,
+        },
+      }
+    end
+    for _, language in ipairs { "typescript", "javascript", "svelte" } do
+      require("dap").configurations[language] = {
+        -- attach to a node process that has been started with
+        -- `--inspect` for longrunning tasks or `--inspect-brk` for short tasks
+        -- npm script -> `node --inspect-brk ./node_modules/.bin/vite dev`
+        {
+          -- use nvim-dap-vscode-js's pwa-node debug adapter
+          type = "pwa-node",
+          -- attach to an already running node process with --inspect flag
+          -- default port: 9222
+          request = "attach",
+          -- allows us to pick the process using a picker
+          processId = require("dap.utils").pick_process,
+          -- name of the debug action you have to select for this config
+          name = "Attach debugger to existing `node --inspect` process",
+          -- for compiled languages like TypeScript or Svelte.js
+          sourceMaps = true,
+          -- resolve source maps in nested locations while ignoring node_modules
+          resolveSourceMapLocations = {
+            "${workspaceFolder}/**",
+            "!**/node_modules/**",
+          },
+          -- path to src in vite based projects (and most other projects as well)
+          cwd = "${workspaceFolder}/src",
+          -- we don't want to debug code inside node_modules, so skip it!
+          skipFiles = { "${workspaceFolder}/node_modules/**/*.js" },
+        },
+        {
+          type = "pwa-chrome",
+          name = "Launch Chrome to debug client",
+          request = "launch",
+          url = "http://localhost:5173",
+          sourceMaps = true,
+          protocol = "inspector",
+          port = 9222,
+          webRoot = "${workspaceFolder}/src",
+          -- skip files from vite's hmr
+          skipFiles = { "**/node_modules/**/*", "**/@vite/*", "**/src/client/*", "**/src/*" },
+        },
+        -- only if language is javascript, offer this debug action
+        language == "javascript"
+        and {
+          -- use nvim-dap-vscode-js's pwa-node debug adapter
+          type = "pwa-node",
+          -- launch a new process to attach the debugger to
+          request = "launch",
+          -- name of the debug action you have to select for this config
+          name = "Launch file in new node process",
+          -- launch current file
+          program = "${file}",
+          cwd = "${workspaceFolder}",
+        }
+        or nil,
+      }
+    end
 
     -- Set virtual text when debugging
     require("nvim-dap-virtual-text").setup {}
@@ -120,7 +147,7 @@ return {
       },
       floating = {
         max_height = 0.9,
-        max_width = 0.5, -- Floats will be treated as percentage of your screen.
+        max_width = 0.5,             -- Floats will be treated as percentage of your screen.
         border = vim.g.border_chars, -- Border style. Can be 'single', 'double' or 'rounded'
         mappings = {
           close = { "q", "<Esc>" },
@@ -144,9 +171,9 @@ return {
     { "<leader>dg", ":lua require('dap').goto_(vim.api.nvim_win_get_cursor(0)[1])<cr>" },
     { "<leader>dl", "<cmd>lua require'dap'.run_last()<cr>" },
     { "<leader>dt", "<cmd>lua require'dap'.terminate()<cr>" },
-    { "<F5>", ":DapContinue<cr>" },
-    { "<F10>", ":DapStepOver<cr>" },
-    { "<F11>", ":DapStepInto<cr>" },
-    { "<S-F11>", ":DapStepOut<cr>" },
+    { "<F5>",       ":DapContinue<cr>" },
+    { "<F10>",      ":DapStepOver<cr>" },
+    { "<F11>",      ":DapStepInto<cr>" },
+    { "<S-F11>",    ":DapStepOut<cr>" },
   },
 }
