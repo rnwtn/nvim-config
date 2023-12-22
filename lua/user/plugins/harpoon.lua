8bd64671251c15885cbafc55bcf6925b4d34566e
rnwtn <77465934+rnwtn@users.noreply.github.com>
2023-12-21 22:52:55 -0600

    Apply formatting

--- lua/user/plugins/harpoon.lua
+++ lua/user/plugins/harpoon.lua
@@ -19,17 +19,17 @@ return {
     end)
   end,
   keys = {
-    { "<leader>a", ":lua require('harpoon'):list():append()<cr>", desc = "Add buffer to Harpoon list" },
+    { "<leader>a", ":lua require('harpoon'):list():append()<cr>",  desc = "Add buffer to Harpoon list" },
     {
       "<leader>s",
       ":lua require('harpoon').ui:toggle_quick_menu(require('harpoon'):list())<cr>",
       desc = "Open Harpoon quick menu",
     },
-    { "<leader>u", ":lua require('harpoon'):list():select(1)<cr>",         desc = "Navigate to Harpoon file 1" },
-    { "<leader>i", ":lua require('harpoon'):list():select(2)<cr>",         desc = "Navigate to Harpoon file 2" },
-    { "<leader>o", ":lua require('harpoon'):list():select(3)<cr>",         desc = "Navigate to Harpoon file 3" },
-    { "<leader>p", ":lua require('harpoon'):list():select(4)<cr>",         desc = "Navigate to Harpoon file 4" },
-    { "<leader>[", ":lua require('harpoon'):list():prev()<cr>",         desc = "Navigate to Harpoon previous file" },
-    { "<leader>]", ":lua require('harpoon'):list():next()<cr>",         desc = "Navigate to Harpoon next file" },
+    { "<leader>u", ":lua require('harpoon'):list():select(1)<cr>", desc = "Navigate to Harpoon file 1" },
+    { "<leader>i", ":lua require('harpoon'):list():select(2)<cr>", desc = "Navigate to Harpoon file 2" },
+    { "<leader>o", ":lua require('harpoon'):list():select(3)<cr>", desc = "Navigate to Harpoon file 3" },
+    { "<leader>p", ":lua require('harpoon'):list():select(4)<cr>", desc = "Navigate to Harpoon file 4" },
+    { "<leader>[", ":lua require('harpoon'):list():prev()<cr>",    desc = "Navigate to Harpoon previous file" },
+    { "<leader>]", ":lua require('harpoon'):list():next()<cr>",    desc = "Navigate to Harpoon next file" },
   },
 }
