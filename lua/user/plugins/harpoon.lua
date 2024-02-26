8bfd7880b3747939170a53d6d7c0de7843eef755
rnwtn <77465934+rnwtn@users.noreply.github.com>
2024-02-26 17:58:21 -0600

    Make harpoon quick menu command silent

--- lua/user/plugins/harpoon.lua
+++ lua/user/plugins/harpoon.lua
@@ -1,4 +1,3 @@
-local toggleQuickMenu = ":lua require('harpoon').ui:toggle_quick_menu(require('harpoon'):list())<cr>"
 return {
   "ThePrimeagen/harpoon",
   event = "BufEnter",
@@ -10,13 +9,18 @@ return {
     },
   },
   keys = {
-    { "<leader>a", ":lua require('harpoon'):list():append()<cr>",  desc = "Add buffer to Harpoon list" },
-    { "<leader>s", toggleQuickMenu,                                desc = "Open Harpoon quick menu" },
     { "<leader>u", ":lua require('harpoon'):list():select(1)<cr>", desc = "Navigate to Harpoon file 1" },
     { "<leader>i", ":lua require('harpoon'):list():select(2)<cr>", desc = "Navigate to Harpoon file 2" },
     { "<leader>o", ":lua require('harpoon'):list():select(3)<cr>", desc = "Navigate to Harpoon file 3" },
     { "<leader>p", ":lua require('harpoon'):list():select(4)<cr>", desc = "Navigate to Harpoon file 4" },
     { "<leader>9", ":lua require('harpoon'):list():prev()<cr>",    desc = "Navigate to Harpoon previous file" },
     { "<leader>0", ":lua require('harpoon'):list():next()<cr>",    desc = "Navigate to Harpoon next file" },
+    { "<leader>a", ":lua require('harpoon'):list():append()<cr>",  desc = "Add buffer to Harpoon list" },
+    {
+      "<leader>s",
+      ":lua require('harpoon').ui:toggle_quick_menu(require('harpoon'):list())<cr>",
+      desc = "Open Harpoon quick menu",
+      silent = true,
+    },
   },
 }
