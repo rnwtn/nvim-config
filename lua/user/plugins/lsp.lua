bf4d05a367bd091095d2a58ddbc9bad0534d823f
rnwtn <77465934+rnwtn@users.noreply.github.com>
2024-05-27 17:20:55 -0500

    Add borders to hover menu

--- lua/user/plugins/lsp.lua
+++ lua/user/plugins/lsp.lua
@@ -121,6 +121,10 @@ return {
       end,
     })
 
+    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+      border = "rounded",
+      title = "hover",
+    })
     vim.diagnostic.config({
       float = { border = "rounded" },
     })
