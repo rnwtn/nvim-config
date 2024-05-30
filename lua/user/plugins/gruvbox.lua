0a368b5471ca3028d45cc1b9e0617c327e1841be
rnwtn <77465934+rnwtn@users.noreply.github.com>
2024-05-29 23:06:26 -0500

    Remove unused colorschemes

--- lua/user/plugins/gruvbox.lua
+++ lua/user/plugins/gruvbox.lua
@@ -1,5 +1,4 @@
 return {
-  {
     "ellisonleao/gruvbox.nvim",
     name = "gruvbox",
     lazy = false,
@@ -41,65 +40,6 @@ return {
         end,
       })
     end,
-  },
-  {
-    "navarasu/onedark.nvim",
-    name = "onedark",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("onedark").setup({
-        -- Main options --
-        style = "deep", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-        transparent = true, -- Show/hide background
-        term_colors = true, -- Change terminal color as per the selected theme style
-        ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
-        cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
-
-        -- toggle theme style ---
-        toggle_style_key = "<leader>ts", -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
-        toggle_style_list = { "dark", "darker", "cool", "deep", "warm", "warmer", "light" }, -- List of styles to toggle between
-
-        -- Change code style ---
-        -- Options are italic, bold, underline, none
-        -- You can configure multiple style with comma seperated, For e.g., keywords = 'italic,bold'
-        code_style = {
-          comments = "none",
-          keywords = "none",
-          functions = "none",
-          strings = "none",
-          variables = "none",
-        },
-
-        -- Lualine options --
-        lualine = {
-          transparent = false, -- lualine center bar transparency
-        },
-
-        -- Custom Highlights --
-        colors = {}, -- Override default colors
-        highlights = {
-          NormalFloat = { bg = "none" },
-          FloatBorder = { bg = "none", fg = "$fg" },
-          TelescopeBorder = { fg = "$fg" },
-          TelescopeNormal = { fg = "$fg" },
-          TelescopePreviewBorder = { fg = "$fg" },
-          TelescopePromptBorder = { fg = "$fg" },
-          TelescopeResultsBorder = { fg = "$fg" },
-          TelescopePromptPrefix = { fg = "$red" },
-          TelescopeSelectionCaret = { fg = "$red" },
-          LspInfoBorder = { fg = "$fg" },
-        },
-
-        -- Plugins Config --
-        diagnostics = {
-          darker = true, -- darker colors for diagnostic
-          undercurl = true, -- use undercurl instead of underline for diagnostics
-          background = true, -- use background color for virtual text
-        },
-      })
-    end,
-  },
 }
 
 -- For NvChad like telescope theme, do something like the following
@@ -109,6 +49,7 @@ return {
 --local telescope_fg = "#f2e5bc"
 --local telescope_green = "#b8bb26"
 --local telescope_red = "#fb4934"
+--
 --TabLineFill = { bg = dimmed_bg },
 --TelescopeBorder = { fg = telescope_bg, bg = telescope_bg },
 --TelescopeNormal = { bg = telescope_bg2 },
