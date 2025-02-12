f6d665c331f0f821dd1499f971b7598ac0406190
rnwtn <77465934+rnwtn@users.noreply.github.com>
2025-02-12 16:50:35 -0600

    Add capabilities do default lsp setup

--- lua/plugins/nvim_lspconfig.lua
+++ lua/plugins/nvim_lspconfig.lua
@@ -25,7 +25,9 @@ return {
 			-- and will be called for each installed server that doesn't have
 			-- a dedicated handler.
 			function(server_name) -- default handler (optional)
-				require("lspconfig")[server_name].setup({})
+				require("lspconfig")[server_name].setup({
+					capabilities = capabilities,
+        })
 			end,
 			-- Next, you can provide a dedicated handler for specific servers.
 			-- For example, a handler override for the `rust_analyzer`:
