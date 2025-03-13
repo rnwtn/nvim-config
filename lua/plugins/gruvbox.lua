ecdfe1ba4ebca487b80c490799076e275738a2cc
rnwtn <77465934+rnwtn@users.noreply.github.com>
2025-03-12 23:16:54 -0500

    Remove custom overrides to gruvbox

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -2,9 +2,9 @@ return {
   "ellisonleao/gruvbox.nvim",
   priority = 1000,
   opts = {
-    transparent_mode = true,
-    overrides = {
-      Pmenu = { bg = "NONE" },
-    },
+    -- transparent_mode = true,
+    -- overrides = {
+    --   Pmenu = { bg = "NONE" },
+    -- },
   },
 }
