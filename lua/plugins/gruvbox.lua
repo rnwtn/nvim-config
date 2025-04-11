d23d0e259cd01df1f3f85ec76de73281598d08ca
rnwtn <77465934+rnwtn@users.noreply.github.com>
2025-04-10 23:04:51 -0500

    Make gruvbox darker

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -6,5 +6,13 @@ return {
     -- overrides = {
     --   Pmenu = { bg = "NONE" },
     -- },
+    -- invert_selection = true,
+    palette_overrides = {
+      dark0 = "#171819",
+      dark1 = "#282828",
+      dark2 = "#3c3836",
+      dark3 = "#504945",
+      dark4 = "#665c54",
+    },
   },
 }
