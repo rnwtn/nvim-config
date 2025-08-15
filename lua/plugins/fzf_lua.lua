dcbdcbf09bef7a72c4c39ee3bc5801346e7deccd
rnwtn <77465934+rnwtn@users.noreply.github.com>
2025-08-15 16:44:08 -0500

    Register FzfLua as ui select

--- lua/plugins/fzf_lua.lua
+++ lua/plugins/fzf_lua.lua
@@ -1,27 +1,30 @@
 return {
   "ibhagwan/fzf-lua",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {
-    winopts = {
-      border = "single", -- :h nvim_open_win()
-      backdrop = 100,
-      preview = {
-        layout = "vertical",
+  config = function()
+    require("fzf-lua").setup({
+      winopts = {
         border = "single", -- :h nvim_open_win()
+        backdrop = 100,
+        preview = {
+          layout = "vertical",
+          border = "single", -- :h nvim_open_win()
+        },
       },
-    },
-    keymap = {
-      builtin = {
-        ["<C-d>"] = "preview-page-down",
-        ["<C-u>"] = "preview-page-up",
+      keymap = {
+        builtin = {
+          ["<C-d>"] = "preview-page-down",
+          ["<C-u>"] = "preview-page-up",
+        },
       },
-    },
-  },
+    })
+    vim.cmd([[FzfLua register_ui_select]])
+  end,
   keys = {
     { "<leader>ff", "<cmd>FzfLua files<cr>", desc = "find files" },
     { "<leader>fb", "<cmd>FzfLua buffers<cr>", desc = "find open buffers" },
     { "<leader>fg", "<cmd>FzfLua live_grep<cr>", desc = "live grep" },
-    { "<leader>fg", "<cmd>FzfLua grep_visual<cr>", desc = "grep visual selection", mode = {"v"}},
+    { "<leader>fg", "<cmd>FzfLua grep_visual<cr>", desc = "grep visual selection", mode = { "v" } },
     { "<leader>fw", "<cmd>FzfLua grep_cword<cr>", desc = "grep word under cursor" },
     { "<leader>fW", "<cmd>FzfLua grep_cWORD<cr>", desc = "grep WORD under cursor" },
     { "<leader>fc", "<cmd>FzfLua colorschemes<cr>", desc = "change colorscheme" },
