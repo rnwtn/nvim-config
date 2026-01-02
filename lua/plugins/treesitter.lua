62b67e9d1bba8aad0fcab9e4834a5095009b6c3f
rnwtn <77465934+rnwtn@users.noreply.github.com>
2026-01-01 20:53:54 -0600

    Update plugins

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,32 +1,42 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  event = "BufReadPost",
-  dependencies = {
-    "JoosepAlviste/nvim-ts-context-commentstring",
-    "nvim-tree/nvim-web-devicons",
-    "windwp/nvim-ts-autotag",
-    "nvim-treesitter/nvim-treesitter-textobjects",
-  },
+  branch = "master",
+  lazy = false,
+  build = ":TSUpdate",
   config = function()
-    vim.g.skip_ts_context_commentstring_module = true
     require("nvim-treesitter.configs").setup({
-      -- A list of parser names, or "all"
-      -- ensure_installed = { "c", "lua", "rust" },
+      -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+
       -- Install parsers synchronously (only applied to `ensure_installed`)
-      -- sync_install = false,
+      sync_install = false,
 
       -- Automatically install missing parsers when entering buffer
+      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
       auto_install = true,
 
-      -- List of parsers to ignore installing (for "all")
-      -- ignore_install = { "javascript" },
+      -- List of parsers to ignore installing (or "all")
+      ignore_install = { "javascript" },
+
+      ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+      -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
       highlight = {
-        -- `false` will disable the whole extension
         enable = true,
 
+        -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+        -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+        -- the name of the parser)
         -- list of language that will be disabled
-        -- disable = { "c", "rust" },
+        --disable = { "c", "rust" },
+        -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+        --disable = function(lang, buf)
+        --  local max_filesize = 100 * 1024 -- 100 KB
+        --  local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        --  if ok and stats and stats.size > max_filesize then
+        --    return true
+        --  end
+        --end,
 
         -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
         -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
@@ -34,45 +44,6 @@ return {
         -- Instead of true it can also be a list of languages
         additional_vim_regex_highlighting = false,
       },
-      autopairs = {
-        enable = true,
-      },
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true,
-          keymaps = {
-            -- You can use the capture groups defined in textobjects.scm
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
-            ["ic"] = "@class.inner",
-            ["aa"] = "@parameter.outer",
-            ["ia"] = "@parameter.inner",
-          },
-          selection_modes = {
-            ["@parameter.outer"] = "v",
-            ["@function.outer"] = "V",
-            ["@class.outer"] = "V",
-          },
-        },
-
-        move = {
-          enable = true,
-          set_jumps = true, -- whether to set jumps in the jumplist
-          goto_next_start = {
-            ["]a"] = "@parameter.inner",
-            ["]f"] = "@function.inner",
-            ["]c"] = "@class.inner",
-          },
-          goto_next_end = {
-            ["]A"] = "@parameter.outer",
-            ["]f"] = "@function.outer",
-            ["]C"] = "@class.outer",
-          },
-        },
-      },
     })
-    require("nvim-ts-autotag").setup()
   end,
 }
